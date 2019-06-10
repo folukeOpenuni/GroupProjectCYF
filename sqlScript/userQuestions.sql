87b5a6d65b7603a8dd91046ea1a07b4643ea7397
folukeOpenuni <folukeagbede@googlemail.com>
2019-06-10 15:50:38 +0100

    change userQuestion to volunteerQuestions

--- sqlScript/userQuestions.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE userQuestions (
-    id 		SERIAL PRIMARY KEY,
-    userID INTEGER NOT NULL,
-    communityChangeAnswer VARCHAR,
-    impactCommunityAnswer VARCHAR,
-    skillAnswers VARCHAR,
-  CONSTRAINT  user_questions_fk FOREIGN KEY (userID) REFERENCES users(id)
-);
