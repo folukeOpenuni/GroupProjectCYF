59859db2a1900d640a0cf47bd233e52595b9e0ac
folukeOpenuni <folukeagbede@googlemail.com>
2019-06-10 15:52:46 +0100

    change table name from users to volunteers

--- sqlScript/users.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-    id 		SERIAL PRIMARY KEY,
-    firstName    VARCHAR(30) NOT NULL,
-    lastName    VARCHAR(30) NOT NULL,    
-    email 	VARCHAR(120) NOT NULL,
-    phone 	VARCHAR(20)
-)
