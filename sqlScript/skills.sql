84fa1caf73f6145c086a1cda7535ac55da2be3ff
folukeOpenuni <folukeagbede@googlemail.com>
2019-06-15 12:41:12 +0100

    add language code to skill

--- sqlScript/skills.sql
+++ sqlScript/skills.sql
@@ -1,5 +1,10 @@
 CREATE TABLE skills (
     id 		SERIAL PRIMARY KEY,
     skillName varchar(100),
-    description varchar(3000)
+    description varchar(3000),
+    teachable char(1),
+    language char(2),
+    CONSTRAINT skill_lang_fk
+        FOREIGN KEY (language)
+        REFERENCES languages(langcode)
 );
