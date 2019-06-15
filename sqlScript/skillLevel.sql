d8351245ad0fc095966b8a258777cc0c1958e32a
folukeOpenuni <folukeagbede@googlemail.com>
2019-06-15 13:23:39 +0100

    fix table name in fk

--- sqlScript/skillLevel.sql
+++ sqlScript/skillLevel.sql
@@ -1,11 +1,11 @@
-CREATE TABLE skillLevel (
+CREATE TABLE skillLevels (
     volunteerID INTEGER NOT NULL,
     skillID INTEGER,
     skillLevel INTEGER,
-    CONSTRAINT PRIMARY KEY (volunteerID, skillID) --makes this column the primary key
+    CONSTRAINT skill_level_pk PRIMARY KEY (volunteerID, skillID), --makes this column the primary key
   CONSTRAINT  volunteer_skill_fk 
     FOREIGN KEY (volunteerID)
-    REFERENCES volunteer(id),
+    REFERENCES volunteers(id),
   CONSTRAINT skill_level_fk
      FOREIGN KEY (skillID)
       REFERENCES skills (id)
