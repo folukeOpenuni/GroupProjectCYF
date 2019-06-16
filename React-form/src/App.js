5b04e2d11d281c1f80c27872c7cc7d9f451bbdee
Ibraham <ibrahim00800@hotmail.com>
2019-06-16 16:20:34 +0100

    2nd commit

--- React-form/src/App.js
+++ React-form/src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
       </header>
       < NameForm />
       
+      
     </div>
     
   );
---
+++ React-form/src/radio.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Radio} from 'semantic-ui-react'
+
+const RadioExampleToggle = () => <Radio toggle 
+
+    onActivate={() => this.setState({ active: true })}
+    onDeactivate={() => this.setState({ active: false })}/>
+   
+
+export default RadioExampleToggle;
