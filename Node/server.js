1e4bfad9dc803cda158b736c6d94dc8b00cbac10
folukeOpenuni <folukeagbede@googlemail.com>
2019-06-19 12:06:47 +0100

    add react app called client

--- Node/server.js
+++ Node/server.js
@@ -1,8 +1,31 @@
 const express = require("express");
 const app = express();
+const Pool = require("pg").Pool; // initialise a connection pool
+
+const db = new Pool({
+  // establish a new connection
+  user: "", // Your user name
+  host: "",
+  database: "", // ... username again.
+  password: "",
+  port: 5432
+});
+
+app.use(express.static("public"));
+
+app.get("/", (request, response) => {
+  db.query(
+    "SELECT firstname, lastname, email FROM volunteers",
+    (error, result) => {
+      result.rows.forEach(row => {
+        console.log(row.firstname, row.lastname, row.email);
+      });
+    }
+  );
+});
+
+app.post("/api", (request, response) => {});
 
 app.listen(3000, () => {
   console.log("server started on port 3000.");
 });
-
-app.use(express.static("public"));
