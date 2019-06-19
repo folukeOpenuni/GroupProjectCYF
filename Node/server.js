249f518d0a63aa7098959435baf333ff955ba0bd
Wasan Sulaiman <wsulaima@jaguarlandrover.com>
2019-06-19 14:40:05 +0100

    refactore package.json and serverjs

--- Node/server.js
+++ Node/server.js
@@ -1,28 +1,33 @@
 const express = require("express");
+const cors = require('cors');
+// const Pool = require("pg").Pool; // initialise a connection pool
+
+// const db = new Pool({
+//   // establish a new connection
+//   user: "cyf", // Your user name
+//   host: "localhost",
+//   database: "cyf", // ... username again.
+//   password: "cyf",
+//   port: 5432
+// });
+
 const app = express();
-const Pool = require("pg").Pool; // initialise a connection pool
-
-const db = new Pool({
-  // establish a new connection
-  user: "cyf", // Your user name
-  host: "localhost",
-  database: "cyf", // ... username again.
-  password: "cyf",
-  port: 5432
-});
-
+app.use(cors());
 app.use(express.static("public"));
 
-app.get("/", (request, response) => {
-  db.query(
-    "SELECT firstname, lastname, email FROM volunteers",
-    (error, result) => {
-      result.rows.forEach(row => {
-        console.log(row.firstname, row.lastname, row.email);
-      });
-    }
-  );
-});
+app.get('/hello', (req, res) => {
+  res.send('Hello World!');
+})
+// app.get("/", (request, response) => {
+//   db.query(
+//     "SELECT firstname, lastname, email FROM volunteers",
+//     (error, result) => {
+//       result.rows.forEach(row => {
+//         console.log(row.firstname, row.lastname, row.email);
+//       });
+//     }
+//   );
+// });
 
 app.post("/api", (request, response) => {});
 
