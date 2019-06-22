c61862525171c7af4fbdccdb9c7cb8e099aaf36f
folukeOpenuni <folukeagbede@googlemail.com>
2019-06-22 17:23:03 +0100

    add body parser

--- Node/server.js
+++ Node/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const Pool = require("pg").Pool; // initialise a connection pool
 const db = new Pool({
   // establish a new connection
@@ -11,6 +12,7 @@ const db = new Pool({
 });
 
 const app = express();
+app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static("public"));
 app.get("/volunteers", (request, response) => {
@@ -35,8 +37,46 @@ app.get("/", (request, response) => {
   );
 });
 
-app.post("/api", (request, response) => {});
+app.get("/locations", (request, response) => {
+  db.query("SELECT id, city, country FROM locations", (error, result) => {
+    response.status(200).json({ locations: result.rows });
+  });
+});
 
-app.listen(3000, () => {
+app.post("/volunteers", (request, response) => {
+  let fn = request.body.firstName;
+  let ls = request.body.lastName;
+  let em = request.body.email;
+  let ph = request.body.phone;
+  let wea = request.body.weekendAvailability;
+  let wda = request.body.weekdayAvailability;
+  let oa = request.body.otherAvailability;
+  let ca = request.body.classAvailability;
+  let os = request.body.otherSkills;
+  //let li = request.body.locationID;
+  let st = request.body.status;
+  let sd = request.body.submissionDate;
+  let cm = request.body.comments;
+  let wed = request.body.welcomeEmailDate;
+  let oed = request.body.onboardEmailDate;
+
+  let sql =
+    "INSERT INTO volunteers (firstName, lastName, email, phone, status)" +
+    " VALUES ($1, $2, $3, $4, $5)" +
+    " RETURNING id";
+
+  db.query(sql, [fn, ls, em, ph, st], (err, result) => {
+    if (err) {
+      response.status(500).json({ error: err });
+    } else {
+      let newID = result.row[0].id;
+      response.status(200).json({
+        id: newID
+      });
+    }
+  });
+});
+
+app.listen(5000, () => {
   console.log("server started on port 3000.");
 });
