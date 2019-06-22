fbbc4e0da64e1cb90bdb6557e6b6ac7d0f0206f9
folukeOpenuni <folukeagbede@googlemail.com>
2019-06-22 17:25:09 +0100

    made app a class and add axios http

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    location: []
+  };
+
+  componentDidMount() {
+    axios.get(`http://localhost:5000/locations`).then(res => {
+      const locations = res.data;
+      console.log(locations);
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// axios.get(`http://localhost:3000/locations`).then(res => {
+//   const locations = res.data;
+//   this.setState({ locations });
+// });
