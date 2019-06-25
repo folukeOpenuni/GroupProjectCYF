acce510c70c98a3ae0d484ddf0150741f52eb6eb
folukeOpenuni <folukeagbede@googlemail.com>
2019-06-25 17:57:33 +0100

    change display

--- dashboard/src/Components/DashboardCard.js
+++ dashboard/src/Components/DashboardCard.js
@@ -6,12 +6,6 @@ import Card from "react-bootstrap/Card";
 export default class DashboardCard extends React.Component {
   constructor() {
     super();
-
-    //       {
-    //           status.map(name => (
-    //               <li>{name}</li>
-    //           ))
-    //       }
   }
   render() {
     let status = [
@@ -25,10 +19,34 @@ export default class DashboardCard extends React.Component {
     return (
       //<CardDeck>
       <CardGroup>
-        <Card>
+        {/* <Card>
           {status.map(header => (
             <Card.Header>{header}</Card.Header>
           ))}
+          <Card.Body />
+        </Card> */}
+        <Card>
+          <Card.Header>Signed Up</Card.Header>
+          <Card.Body />
+        </Card>
+        <Card>
+          <Card.Header>Contacted</Card.Header>
+          <Card.Body />
+        </Card>
+        <Card>
+          <Card.Header>Replied Contact</Card.Header>
+          <Card.Body />
+        </Card>
+        <Card>
+          <Card.Header>Intro Chat</Card.Header>
+          <Card.Body />
+        </Card>
+        <Card>
+          <Card.Header>Attended a class</Card.Header>
+          <Card.Body />
+        </Card>
+        <Card>
+          <Card.Header>Intro to Volunteering</Card.Header>
           <Card.Body />
         </Card>
       </CardGroup>
