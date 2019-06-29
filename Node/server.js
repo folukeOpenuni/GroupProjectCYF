84edc60a9bec3bb5403de9d30299533899c468ff
folukeOpenuni <folukeagbede@googlemail.com>
2019-06-29 15:07:37 +0100

    change port to 3000 because of error

--- Node/server.js
+++ Node/server.js
@@ -93,6 +93,6 @@ app.post("/volunteers", (request, response) => {
   });
 });
 
-app.listen(5000, () => {
-  console.log("server starting on port 5000......");
+app.listen(3000, () => {
+  console.log("server starting on port 3000......");
 });
