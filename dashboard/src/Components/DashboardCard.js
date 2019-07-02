725aad2b9c110039e612efb499c78451e1c88aa5
folukeOpenuni <folukeagbede@googlemail.com>
2019-07-02 12:39:14 +0100

    import modal and button element

--- dashboard/src/Components/DashboardCard.js
+++ dashboard/src/Components/DashboardCard.js
@@ -3,18 +3,31 @@ import React from "react";
 import CardGroup from "react-bootstrap/CardGroup";
 import Card from "react-bootstrap/Card";
 import axios from "axios";
+import Modals from "./Modals";
+import Button from "react-bootstrap/Button";
+import ButtonToolbar from "react-bootstrap/ButtonToolbar";
 
 export default class DashboardCard extends React.Component {
   constructor() {
     super();
-    this.state = { volunteers: [] };
+    this.state = { volunteers: [], modalShow: false };
   }
+
   componentDidMount() {
     axios.get("http://localhost:5000/dashboardName").then(result => {
       console.log(result.data.volunteers);
       this.setState({ volunteers: result.data.volunteers });
     });
   }
+
+  renderCard() {
+    console.log(this.state);
+    this.state.volunteers.forEach(volunteer => {
+      console.log("vols:", volunteer);
+      return { volunteer };
+    });
+  }
+
   render() {
     let status = [
       "Signed Up",
@@ -24,23 +37,40 @@ export default class DashboardCard extends React.Component {
       "Attended a class",
       "Intro to Volunteering"
     ];
+    let modalClose = () => this.setState({ modalShow: false });
+
     return (
       //<CardDeck>
       <CardGroup>
+        {/* {this.renderCard()} */}
         {/* <Card>
           {status.map(header => (
             <Card.Header>{header}</Card.Header>
           ))}
           <Card.Body />
         </Card> */}
+
         <Card>
           <Card.Header>Signed Up</Card.Header>
           <Card.Body>
-            <ul>
+            <React.Fragment>{this.renderCard()}</React.Fragment>
+            <Button
+              variant="primary"
+              onClick={() => this.setState({ modalShow: true })}
+            >
+              {/* <ul>
+                  {this.state.volunteers.map(vol => (
+                    <li>{vol.firstname}</li>
+                  ))}
+                </ul>{" "} */}
+            </Button>
+
+            <Modals show={this.state.modalShow} onHide={modalClose} />
+            {/* <ul>
               {this.state.volunteers.map(vol => (
                 <li>{vol.firstname}</li>
               ))}
-            </ul>
+            </ul> */}
           </Card.Body>
         </Card>
         <Card>
