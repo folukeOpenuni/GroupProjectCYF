00d5ad1942ace127463910ecc4e2095d64de9b2b
folukeOpenuni <folukeagbede@googlemail.com>
2019-07-05 14:01:18 +0100

    update volunteer endpoint

--- Node/server.js
+++ Node/server.js
@@ -69,28 +69,39 @@ app.post("/volunteers", (request, response) => {
   let oa = request.body.otherAvailability;
   let ca = request.body.classAvailability;
   let os = request.body.otherSkills;
-  //let li = request.body.locationID;
+  let li = request.body.locationID;
   let st = request.body.status;
-  let sd = request.body.submissionDate;
-  let cm = request.body.comments;
-  let wed = request.body.welcomeEmailDate;
-  let oed = request.body.onboardEmailDate;
+  // let sd = request.body.submissionDate;
+  //let cm = request.body.comments;
+  // let wed = request.body.welcomeEmailDate;
+  // let oed = request.body.onboardEmailDate;
 
   let sql =
-    "INSERT INTO volunteers (firstName, lastName, email, phone, status)" +
-    " VALUES ($1, $2, $3, $4, $5)" +
+    "INSERT INTO volunteers (firstName, lastName, email, phone," +
+    " weekendAvailability, weekdayAvailability, otherAvailability, " +
+    " classAvailability, otherSkills, locationID, status, submissionDate)" +
+    " VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, current_time)" +
     " RETURNING id";
 
-  db.query(sql, [fn, ls, em, ph, st], (err, result) => {
-    if (err) {
-      response.status(500).json({ error: err });
-    } else {
-      let newID = result.row[0].id;
-      response.status(200).json({
-        id: newID
-      });
+  db.query(
+    sql,
+    [fn, ls, em, ph, wea, wda, oa, ca, os, li, st],
+    (err, result) => {
+      if (err) {
+        response.status(500).json({ error: err });
+      } else {
+        let newID = result.row[0].id;
+        let sql2 =
+          "INSERT INTO skillLevels (volunteerID, skillID, skillLevel)" +
+          " VALUES ($1, $2, $3)";
+        //db.query(sql2, [newID]) //this query needs to be inside a loop
+        //because volunteers can have multiple skills
+        response.status(200).json({
+          id: newID //returns volunteer id
+        });
+      }
     }
-  });
+  );
 });
 
 const SERVER_PORT = process.env.PORT || 3000;
