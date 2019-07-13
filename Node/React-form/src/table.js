f682ae04ecad1cee3e57f5101b9a60e82d1b78fb
folukeOpenuni <folukeagbede@googlemail.com>
2019-07-13 16:18:29 +0100

    Anothy solution for getting skill name

--- Node/React-form/src/table.js
+++ Node/React-form/src/table.js
@@ -1,74 +1,97 @@
 import React from "react";
+import axios from "axios";
 import { Header, Table, Rating } from "semantic-ui-react";
 
-const TableExamplePadded = () => (
-  <Table celled padded>
-    <Table.Header>
-      <Table.Row>
-        <Table.HeaderCell singleLine>Programming Language</Table.HeaderCell>
+const skills = [
+  { id: 1, skillname: "HTML & CSS" },
+  { id: 2, skillname: "JavaScript" },
+  { id: 3, skillname: "React" },
+  { id: 4, skillname: "Node/SQL" },
+  { id: 5, skillname: "Agile Methodologies" },
+  { id: 6, skillname: "Graduate Job Placement (Recruitment)" },
+  { id: 7, skillname: "Event Management" },
+  { id: 8, skillname: "Coaching" },
+  { id: 9, skillname: "Accounting / Bookkeeping" },
+  { id: 10, skillname: "Project Management / Business Analysis" },
+  { id: 11, skillname: "Personal Support Work / Wellbeing" },
+  {
+    id: 12,
+    skillname: "NGO and Corporate Outreach / Fundraising / Partnerships"
+  },
+  { id: 13, skillname: "Photography / Videography" },
+  { id: 14, skillname: "Volunteer Engagement / Community Management" },
+  { id: 15, skillname: "Growth Marketing / Social Media Strategy" },
+  { id: 16, skillname: "Journalism / Writing" },
+  { id: 17, skillname: "Pedagogy / Learning Environments" }
+];
 
-        <Table.HeaderCell>Level</Table.HeaderCell>
-      </Table.Row>
-    </Table.Header>
+const skillsRequired = skills.slice(0, 4);
 
-    <Table.Body>
+class TableExamplePadded extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      skillLevel: ""
+    };
+  }
+  onClick(event) {
+    const currentTarget = event.currentTarget;
+    console.log(currentTarget);
+    this.setState({
+      skillLevel: currentTarget.getAttribute("aria-posinset")
+    });
+  }
+  componentDidMount() {
+    axios.get("http://localhost:8000/skill").then(result => {
+      console.log(result.data.skills);
+      this.setState({ skill: result.data.skills });
+      console.log("state", this.state.skills);
+    });
+  }
+
+  render() {
+    const Row = ({ children }) => (
       <Table.Row>
         <Table.Cell>
           <Header as="h4" textAlign="center">
-            HTML/CSS
+            {children}
           </Header>
         </Table.Cell>
 
         <Table.Cell>
-          <Rating icon="star" defaultRating={0} maxRating={5} />
+          <Rating
+            icon="star"
+            defaultRating={0}
+            maxRating={5}
+            onRate={e => this.onClick(e)}
+          />
         </Table.Cell>
       </Table.Row>
-      <Table.Row>
-        <Table.Cell>
-          <Header as="h4" textAlign="center">
-            JavaScript
-          </Header>
-        </Table.Cell>
+    );
+    return (
+      <div>
+        <Table celled padded>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell singleLine>
+                Programming Language
+              </Table.HeaderCell>
 
-        <Table.Cell>
-          <Rating icon="star" defaultRating={0} maxRating={5} />
-        </Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>
-          <Header as="h4" textAlign="center">
-            React
-          </Header>
-        </Table.Cell>
-
-        <Table.Cell>
-          <Rating icon="star" defaultRating={0} maxRating={5} />
-        </Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>
-          <Header as="h4" textAlign="center">
-            Node/SQL
-          </Header>
-        </Table.Cell>
-
-        <Table.Cell>
-          <Rating icon="star" defaultRating={0} maxRating={5} />
-        </Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>
-          <Header as="h4" textAlign="center">
-            Agile Methodologies{" "}
-          </Header>
-        </Table.Cell>
-
-        <Table.Cell>
-          <Rating icon="star" defaultRating={0} maxRating={5} />
-        </Table.Cell>
-      </Table.Row>
-    </Table.Body>
-  </Table>
-);
+              <Table.HeaderCell>Level</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {skillsRequired.map(skill => (
+              <Row>
+                {skill.skillname}
+                <p>{this.state.skillLevel}</p>
+              </Row>
+            ))}
+          </Table.Body>
+        </Table>
+      </div>
+    );
+  }
+}
 
 export default TableExamplePadded;
