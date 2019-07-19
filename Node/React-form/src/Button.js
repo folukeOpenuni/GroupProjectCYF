1c6bc1e7bc810e75b8ad21e4e4e80f0ff0a36cc6
folukeOpenuni <folukeagbede@googlemail.com>
2019-07-19 13:23:01 +0100

    delete commited code

--- Node/React-form/src/Button.js
+++ Node/React-form/src/Button.js
@@ -1,11 +1,3 @@
-//import React from "react";
-
-//const Button = props => {
-// return <button className="btn btn-primary">{props.placeHolder}</button>;
-//};
-
-//export default Button;
-
 import React from "react";
 import { Button, Segment } from "semantic-ui-react";
 
